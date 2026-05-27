808989445ea220d6f2d7cf740e85acb36990a275
Vaibhav Srivastava <51719440+srivaibhavsagar@users.noreply.github.com>
2026-05-27 21:05:17 +0100

    Create main.tf

---
+++ aws/terraform/modules/aws-ec2-instance/main.tf
@@ -0,0 +1,69 @@
+# modules/aws-ec2-instance/main.tf
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name        = "${var.name}-sg"
+  description = "Security group for ${var.name}"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.allowed_ssh_cidrs
+  }
+
+  dynamic "ingress" {
+    for_each = var.additional_ports
+    content {
+      description = "Port ${ingress.value}"
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = var.allowed_ingress_cidrs
+    }
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(var.tags, {
+    Name = "${var.name}-sg"
+  })
+}
+
+resource "aws_instance" "this" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  key_name               = var.key_name
+
+  root_block_device {
+    volume_size = var.root_volume_size
+    volume_type = var.root_volume_type
+    encrypted   = true
+  }
+
+  metadata_options {
+    http_tokens   = "required"  # IMDSv2 enforced
+    http_endpoint = "enabled"
+  }
+
+  tags = merge(var.tags, {
+    Name = var.name
+  })
+}
