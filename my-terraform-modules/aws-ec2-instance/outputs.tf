0ad9815b9488bcbde70f9e439df47cf0a7ffa835
Vaibhav Srivastava <51719440+srivaibhavsagar@users.noreply.github.com>
2026-05-27 21:26:09 +0100

    Create outputs.tf

---
+++ my-terraform-modules/aws-ec2-instance/outputs.tf
@@ -0,0 +1,19 @@
+output "instance_id" {
+  description = "EC2 instance ID"
+  value       = aws_instance.this.id
+}
+
+output "private_ip" {
+  description = "Private IP address"
+  value       = aws_instance.this.private_ip
+}
+
+output "public_ip" {
+  description = "Public IP address (if assigned)"
+  value       = aws_instance.this.public_ip
+}
+
+output "security_group_id" {
+  description = "Security group ID"
+  value       = aws_security_group.instance.id
+}
